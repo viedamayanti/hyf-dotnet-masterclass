aa1dbf9e557370e594bb53f9d7c67795092281a0
viedamayanti <vinaniekdamayanti@gmail.com>
2023-03-23 11:28:30 +0100

    Implement feedback

--- week3/homework-week3/Program.cs
+++ week3/homework-week3/Program.cs
@@ -4,66 +4,72 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 //Task 1 - Temperature
-app.MapGet("/temperature", () => {
-var myDegree = new Temperature(10m);
-System.Console.WriteLine($"{myDegree.Celcius} Celsius is {myDegree.Fahrenheit} Fahrenheit");
+app.MapGet("/temperature", () =>
+{
+    var myDegree = new Temperature(10m);
+    System.Console.WriteLine($"{myDegree.Celcius} Celsius is {myDegree.Fahrenheit} Fahrenheit");
 });
 
 //Task 2 - Exchangerate
-app.MapGet("/exchange", () => {
-var amount = 160;
-var exchangeRate = new ExchangeRate("EUR", "DKK");
-exchangeRate.Rate = 7.5m;
-Console.WriteLine($"The rate of {amount} {exchangeRate.FromCurrency} is {exchangeRate.Calculate(amount)} {exchangeRate.ToCurrency}");
+app.MapGet("/exchange", () =>
+{
+    var amount = 160;
+    var exchangeRate = new ExchangeRate("EUR", "DKK");
+    exchangeRate.Rate = 7.5m;
+    Console.WriteLine($"The rate of {amount} {exchangeRate.FromCurrency} is {exchangeRate.Calculate(amount)} {exchangeRate.ToCurrency}");
 });
 
 //Task 3 - Bank account
-app.MapGet("/account", () =>{
-var amountBalance = new Account(100);
-amountBalance.Deposit(100);
-Console.WriteLine($"Account balance is {amountBalance.Balance}");
-amountBalance.Withdraw(20);
-System.Console.WriteLine($"Account balance is {amountBalance.Balance}");
-// amountBalance.Withdraw(200); //Unhandled exception. System.Exception: Your balance is not enough
+app.MapGet("/account", () =>
+{
+    var amountBalance = new Account(100);
+    amountBalance.Deposit(100);
+    Console.WriteLine($"Account balance is {amountBalance.Balance}");
+    amountBalance.Withdraw(20);
+    System.Console.WriteLine($"Account balance is {amountBalance.Balance}");
+    // amountBalance.Withdraw(200); //Unhandled exception. System.Exception: Your balance is not enough
 });
 
 //Task 4 - Interface
-app.MapGet("/interface", () => {
-var myCat = new Cat();
-MakeSound(myCat);
+app.MapGet("/interface", () =>
+{
+    var myCat = new Cat();
+    MakeSound(myCat);
 
-var myCow = new Cow();
-MakeSound(myCow);
+    var myCow = new Cow();
+    MakeSound(myCow);
+
+    var myDog = new Dog();
+    MakeSound(myDog);
 
 
-var myDog = new Dog();
-MakeSound(myDog);
-
-
-void MakeSound(IAnimal animal){
-    System.Console.WriteLine($"{animal.Name} says {animal.Sound}");
-}
+    void MakeSound(IAnimal animal)
+    {
+        System.Console.WriteLine($"{animal.Name} says {animal.Sound}");
+    }
 });
-
 
 app.Run();
 
 //Temperature
-public class Temperature{
+public class Temperature
+{
     public decimal celDegree = -273.15m;
     public decimal Celcius { get; set; }
 
-    public Temperature(decimal celciusDegree){
+    public Temperature(decimal celciusDegree)
+    {
         Celcius = celciusDegree;
-        if(Celcius < celDegree){
-       throw new Exception();
+        if (Celcius < celDegree)
+        {
+            throw new Exception();
         }
-    } 
+    }
     public string Fahrenheit
     {
         get => ($"{((Celcius * 9) / 5) + 32}");
     }
-    }
+}
 
 
 //Exchange rate
@@ -78,12 +84,13 @@ public class ExchangeRate
     {
         FromCurrency = fromCurrency;
         ToCurrency = toCurrency;
-       
+
     }
     public decimal Calculate(decimal amount)
     {
-              if(Rate <= 0 || amount <= 0){
-            throw new Exception("Must positiv number"); 
+        if (Rate <= 0 || amount <= 0)
+        {
+            throw new Exception("Must positiv number");
         }
         var result = Rate * amount;
         return result;
@@ -94,36 +101,28 @@ public class ExchangeRate
 //Bank account
 public class Account
 {
-   public decimal Amount{ get; set; }
-    public decimal Balance { get; set;}
-  
+    public decimal Amount { get; set; }
+    public decimal Balance { get; set; }
 
     public Account(decimal balance)
     {
-        Balance = balance ;
+        Balance = balance;
 
     }
-         public decimal Deposit(decimal Amount)
+    public decimal Deposit(decimal Amount)
     {
-        {
-            if (Balance > 0)
-            {
-                Balance += Amount;
-            }
-            return Balance;
-        }
+        return Balance += Amount;
     }
 
-     public decimal Withdraw(decimal Amount)
-     {
-     {
-        if (Balance <= Amount)
+    public decimal Withdraw(decimal Amount)
+    {
+
+        if (Balance < Amount)
         {
             throw new Exception("Your balance is not enough");
         }
-            return Balance -= Amount;
-        }
-     }
+        return Balance -= Amount;
+    }
 }
 
 
@@ -144,7 +143,7 @@ class Cat : IAnimal
 class Cow : IAnimal
 {
     public string Name { get => "Cow"; }
-    public string Sound { get =>  "Mooo moooo"; }
+    public string Sound { get => "Mooo moooo"; }
 }
 class Dog : IAnimal
 {
